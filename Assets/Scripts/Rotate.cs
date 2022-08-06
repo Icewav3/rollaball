ad78c909c86c5b6bb459b8f31ca6e318bc9d7484
Icewav3 <coledorman12@gmail.com>
2022-08-06 12:55:49 -0700

    colors

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -1,12 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
+    //public bool random;
     public Vector3 rotationForce = new Vector3(0f,0f,0f);
+
+    /*private void Start()
+    {
+        if (random)
+        {
+            Random rng = new UnityEngine.Random();
+        }
+    }*/
+
     private void Update()
     {
-        transform.Rotate(rotationForce * Time.deltaTime);
+        /*if (random)
+        {
+            transform.Rotate(rotationForce * Time.deltaTime);
+        }
+        else
+        {*/
+            transform.Rotate(rotationForce * Time.deltaTime);
+        //}
     }
 }
