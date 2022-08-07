323b045817108e48ce485f04d0a5e394634abed3
Icewav3 <coledorman12@gmail.com>
2022-08-06 22:01:45 -0700

    Final

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI Score;
+    public GameObject WinTextGameObject;
+    
     private Rigidbody rb;
     private float movementx;
     private float movementy;
-    
+    private int count;
     /*private Vector3 movement;
 
     public Vector3 GetVector()
@@ -22,6 +26,9 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        WinTextGameObject.SetActive(false);
+        setCountText();
     }
 
     void OnMove(InputValue movementValue)
@@ -31,9 +38,28 @@ public class PlayerController : MonoBehaviour
         movementy = movementVector.y;
     }
 
+    private void setCountText()
+    {
+        Score.text = "Current Score: " + count.ToString();
+        if (count >= 12)
+        {
+            WinTextGameObject.SetActive(true);
+        }
+    }
+
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementx, 0.0f, movementy);
         rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            count++;
+            other.gameObject.SetActive(false);
+            setCountText();
+        }
     }
 }
